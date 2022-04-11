de40c98d35298ee874e5970aec92b86553b16393
Claudio Hernandez <93549233+HernandezAriel@users.noreply.github.com>
2022-04-11 11:23:49 -0300

    Add files via upload

---
+++ Day 6/DisenoPatrones/Logger.java
@@ -0,0 +1,25 @@
+package DisenoPatrones;
+
+public class Logger {
+    public static Logger log;
+    private String value;
+    
+    private Logger(){
+        
+    }
+
+    public static Logger getInstance(){
+        if(log == null){
+            log = new Logger();
+        }
+        return log;
+    }
+
+    public void setValue(String msg){
+        this.value = msg;
+    }
+
+    public String getValue(){
+        return value;
+    }
+}
