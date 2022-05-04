07e3ecbea0ceffd7bdb5d7c5353179d2eec037d8
Claudio Hernandez <93549233+HernandezAriel@users.noreply.github.com>
2022-05-04 10:21:57 -0300

    Delete Day22/day22 directory

--- Day22/day22/src/main/java/com/example/day22/controllers/repositories/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.day22.controllers.repositories;
-
-import com.example.day22.models.Game;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface GameRepository extends CrudRepository<Game, Long>{
-     
-}
